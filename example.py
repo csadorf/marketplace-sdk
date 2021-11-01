632c63820450c09e47b5acbe96a0b2f3c48dd6e3
Simon Adorf (EPFL) <simon.adorf@epfl.ch>
2021-11-01 17:19:11 +0000

    Add OPTIMADE app to example.

--- example.py
+++ example.py
@@ -7,3 +7,17 @@ mp = MarketPlace()
 r = mp.get(mp.url_userinfo)
 r.raise_for_status()
 pprint(r.json())
+
+
+OPTIMADE_APP_ID='4909efbc-c61a-4b77-8bdc-6df139f98092'
+r = mp.get(f'/api/proxy/proxy/{OPTIMADE_APP_ID}/heartbeat')
+r.raise_for_status()
+print(r.content)
+
+r = mp.get(f'/api/proxy/proxy/{OPTIMADE_APP_ID}/test')
+r.raise_for_status()
+pprint(r.json())
+
+r = mp.get(f'/api/proxy/proxy/{OPTIMADE_APP_ID}/structures/1')
+r.raise_for_status()
+print(r.content)
