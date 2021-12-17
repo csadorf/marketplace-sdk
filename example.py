abeb7f8262ffb4d817a9459c5483668ee22bdcff
Simon Adorf (EPFL) <simon.adorf@epfl.ch>
2021-12-17 17:25:15 +0000

    Update example.

--- example.py
+++ example.py
@@ -2,21 +2,19 @@ from pprint import pprint
 
 from marketplace import MarketPlace
 
-mp = MarketPlace()
+MP = MarketPlace()
 
 # Show the user information
-response = mp.get(mp.url_userinfo)
-response.raise_for_status()
-pprint(response.json())
+pprint(MP.userinfo())
 
 SOME_MARKEPTLACE_APP_ID = "203522a2-dfac-481c-bd9e-46806b6d06f1"
 
 # Check whether the application can be reached and is alive:
-r = mp.get(f"/api/proxy/proxy/{SOME_MARKEPTLACE_APP_ID}/heartbeat")
+r = MP.get(f"/api/proxy/proxy/{SOME_MARKEPTLACE_APP_ID}/heartbeat")
 r.raise_for_status()
 pprint(r.json())
 
 # List available data sets
-r = mp.get(f"/api/proxy/proxy/{SOME_MARKEPTLACE_APP_ID}/datasets/")
+r = MP.get(f"/api/proxy/proxy/{SOME_MARKEPTLACE_APP_ID}/datasets/")
 r.raise_for_status()
 print(r.content)
